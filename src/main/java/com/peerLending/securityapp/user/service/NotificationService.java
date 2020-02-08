a9394ea254a3209c599b6916f92750377e5a31e9
Mindaugas <mindaugas.barcauskas@mif.stud.vu.lt>
2020-02-08 22:45:11 +0200

    initial commit

---
+++ src/main/java/com/peerLending/securityapp/user/service/NotificationService.java
@@ -0,0 +1,23 @@
+package com.peerLending.securityapp.user.service;
+
+import com.google.gson.Gson;
+import com.peerLending.securityapp.user.model.User;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationService {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final static Gson GSON = new Gson();
+
+    @Autowired
+    public NotificationService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(User user) {
+        rabbitTemplate.convertAndSend("userRegisteredTopic", "user.registered", GSON.toJson(user));
+    }
+}
